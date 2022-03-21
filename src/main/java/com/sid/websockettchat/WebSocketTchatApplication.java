6a8fd7450edf433d71d58b1e700e83d489abd93d
Khaled Hizaoui <Hizaoui@ONGENDA.ENDA>
2022-03-21 10:44:42 +0100

    first commit

---
+++ src/main/java/com/sid/websockettchat/WebSocketTchatApplication.java
@@ -0,0 +1,13 @@
+package com.sid.websockettchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketTchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketTchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sid/websockettchat/WebSocketTchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.websockettchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketTchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
